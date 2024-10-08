db78629a143e5bdfc85ef1f556ad01703aac0751
SP-maXX <maxspeeder12@gmail.com>
2024-10-08 19:56:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MrdFTIbQ#8ew7p6w-7eRGImSzAqIe3hipFQa58XiHul6xqiuJuVE",
+SESSION_ID: process.env.SESSION_ID || "9qFB1D7Z#OqL_XARayh9-VGFyF-1iq6-OXRrNhA1p3k3c1utpaVY",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "╭───────────────────────▢\n│─ Hellow Boss, I am Online Now! 🌚👋\n│─ ʙᴏᴛ : Ｓᴘᴇᴇᴅ ＭᴀＸＸ- ＭＤ\n│─ ᴠᴇʀsɪᴏɴ : 1.0.0\n│─ ᴏᴡɴᴇʀ : Ｓᴀɴᴛʜᴜsʜ.Ｓ\n│─ ɴᴜᴍʙᴇʀ : 94772726437\n│─ ʙᴀsᴇᴅ ᴏɴ : Ｇɪᴛʜᴜʙ\n│─ ᴡᴏʀᴋ ᴛʏᴘᴇ : Ｐᴜʙʟɪᴄ\n│─ ᴘʀᴇғɪx : [ . ]\n╰───────────────────────▢",
